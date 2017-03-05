46d288dae039988e18a6f46d4b60a5dbf1ab2090
Mitchell Suzuki <msuzuki@dealersocket.com>
2017-03-05 17:49:34 -0600

    added git ignore. defined init of scrapper

--- src/scrapper.py
+++ src/scrapper.py
@@ -1,5 +1,10 @@
 from bs4 import BeautifulSoup
+from pycurl import pycurl
+
 
 class Scrapper:
-    def __init__(self):
-        pass
+    def __init__(self, location, position):
+        self.location = location
+        self.position = position
+
+
